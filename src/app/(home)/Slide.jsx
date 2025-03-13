d1a7644559ed529f402664b4f4300b4fae41cd01
chocoscoding <chocoscoding@gmail.com>
2025-03-13 04:41:46 +0100

    added sanity

--- src/app/(home)/Slide.jsx
+++ src/app/(home)/Slide.jsx
@@ -34,7 +34,7 @@ const Slide = () => {
       xPercent: -10,
       scrollTrigger: {
         ...scrollTigger1,
-        scrub: 600, // Increase the scrub value to slow down the animation
+        scrub: 60, // Increase the scrub value to slow down the animation
       },
     });
   });
