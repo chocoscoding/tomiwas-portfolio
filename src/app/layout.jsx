7fd783b0f3bdf9f418e33178a55d982406d036b2
chocoscoding <chocoscoding@gmail.com>
2025-03-14 17:41:03 +0100

    added top loader

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,7 @@ export const metadata = {
   description: "Solarin Ayotomiwa - Creative",
 };
 import SmoothScrolling from "../components/SmoothScroll";
+import NextTopLoader from "nextjs-toploader";
 import { ViewTransitions } from "next-view-transitions";
 import Footer from "../components/Footer";
 export default function RootLayout({ children }) {
@@ -14,6 +15,19 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         {/* <ViewTransitions> */}
+        <NextTopLoader
+          color="#fff7d7"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #fff7d7,0 0 5px #fff7d7"
+          zIndex={1600}
+          showAtBottom={false}
+        />
         <div className="father">
           <CustomCursor />
           <SmoothScrolling>
